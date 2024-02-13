f97b3eac5cf03c7d9ba23b45e932a51027915715
batreeon <1340551309@qq.com>
2024-02-13 23:40:08 +0800

    修改变量名

---
+++ 4/reference/target/debug/deps/reference-483bfc4725022f39.d
@@ -0,0 +1,5 @@
+/home/batreeon/workspace/trpl/4/reference/target/debug/deps/reference-483bfc4725022f39: src/main.rs
+
+/home/batreeon/workspace/trpl/4/reference/target/debug/deps/reference-483bfc4725022f39.d: src/main.rs
+
+src/main.rs:
--- 4/reference/target/debug/reference.d
+++ 4/reference/target/debug/reference.d
@@ -1,1 +1,1 @@
-/Users/bytedance/go/src/github/trpl/4/reference/target/debug/reference: /Users/bytedance/go/src/github/trpl/4/reference/src/main.rs
+/home/batreeon/workspace/trpl/4/reference/target/debug/reference: /home/batreeon/workspace/trpl/4/reference/src/main.rs
