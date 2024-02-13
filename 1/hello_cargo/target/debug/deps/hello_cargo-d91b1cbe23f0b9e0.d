0738148ed16315e7e97b0764617219018a2c428d
batreeon <1340551309@qq.com>
2024-02-13 23:27:02 +0800

    cargo run

---
+++ 1/hello_cargo/target/debug/deps/hello_cargo-d91b1cbe23f0b9e0.d
@@ -0,0 +1,5 @@
+/home/batreeon/workspace/trpl/1/hello_cargo/target/debug/deps/libhello_cargo-d91b1cbe23f0b9e0.rmeta: src/main.rs
+
+/home/batreeon/workspace/trpl/1/hello_cargo/target/debug/deps/hello_cargo-d91b1cbe23f0b9e0.d: src/main.rs
+
+src/main.rs:
--- 1/hello_cargo/target/debug/hello_cargo.d
+++ 1/hello_cargo/target/debug/hello_cargo.d
@@ -1,1 +1,1 @@
-/Users/bytedance/go/src/github/trpl/1/hello_cargo/target/debug/hello_cargo: /Users/bytedance/go/src/github/trpl/1/hello_cargo/src/main.rs
+/home/batreeon/workspace/trpl/1/hello_cargo/target/debug/hello_cargo: /home/batreeon/workspace/trpl/1/hello_cargo/src/main.rs
